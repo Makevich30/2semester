ce66776da1eebff92645e7b802ef60b8a7dfa190
Makevich30 <84179265+Makevich30@users.noreply.github.com>
2024-05-17 00:58:41 +0400

    Create 10-2-8.cpp

---
+++ 10-2-8.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+struct Car {
+    string brand;
+    string number;
+    string ownerLastName;
+    int purchaseYear;
+    int mileage;
+};
+
+// Функция для чтения данных из файла
+void readDataFromFile(const string& filename, Car cars[], int& numCars) {
+    ifstream inputFile(filename);
+    if (!inputFile.is_open()) {
+        cerr << "Unable to open input file" << endl;
+        exit(1);
+    }
+
+    inputFile >> numCars;
+    for (int i = 0; i < numCars; ++i) {
+        inputFile >> cars[i].brand >> cars[i].number >> cars[i].ownerLastName >> cars[i].purchaseYear >> cars[i].mileage;
+    }
+
+    inputFile.close();
+}
+
+// Функция для записи данных в файл
+void writeDataToFile(const string& filename, const Car cars[], int numCars) {
+    ofstream outputFile(filename);
+    if (!outputFile.is_open()) {
+        cerr << "Unable to open output file" << endl;
+        exit(1);
+    }
+
+    outputFile << numCars << endl;
+    for (int i = 0; i < numCars; ++i) {
+        outputFile << cars[i].brand << " " << cars[i].number << " " << cars[i].ownerLastName << " " << cars[i].purchaseYear << " " << cars[i].mileage << endl;
+    }
+
+    outputFile.close();
+}
+
+// Функция для удаления автомобилей с пробегом менее заданного значения
+void removeCarsWithLessMileage(Car cars[], int& numCars, int minMileage) {
+    int j = 0;
+    for (int i = 0; i < numCars; ++i) {
+        if (cars[i].mileage >= minMileage) {
+            cars[j] = cars[i];
+            ++j;
+        }
+    }
+    numCars = j;
+}
+
+int main() {
+    const string inputFile = "input.txt"; // Имя входного файла
+    const string outputFile = "output.txt"; // Имя выходного файла
+    const int minMileage = 100000; // Минимальный пробег для включения в автомобильную ведомость
+
+    const int MAX_CARS = 1000; // Максимальное количество автомобилей
+    Car cars[MAX_CARS];
+    int numCars;
+
+    // Чтение данных из файла
+    readDataFromFile(inputFile, cars, numCars);
+
+    // Удаление автомобилей с пробегом менее заданного значения
+    removeCarsWithLessMileage(cars, numCars, minMileage);
+
+    // Запись данных в файл
+    writeDataToFile(outputFile, cars, numCars);
+
+    cout << "Automobile register created successfully!" << endl;
+
+    return 0;
+}
