b85c675ac2f1a0004e064c67a2c5984d286930f5
Makevich30 <84179265+Makevich30@users.noreply.github.com>
2024-05-17 00:26:45 +0400

    Create 9-1-4.cpp

---
+++ 9-1-4.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <limits>
+
+using namespace std;
+
+int main() {
+    ifstream inputFile("input.txt");
+    ofstream outputFile("output.txt");
+
+    string shortestString;
+    string currentString;
+    int shortestLength = numeric_limits<int>::max();
+
+    while (getline(inputFile, currentString)) {
+        if (currentString.length() < shortestLength) {
+            shortestString = currentString;
+            shortestLength = currentString.length();
+        }
+    }
+
+    if (shortestLength != numeric_limits<int>::max()) {
+        outputFile << "Shortest string: " << shortestString << endl;
+        outputFile << "Length: " << shortestLength << endl;
+    } else
+
+    inputFile.close();
+    outputFile.close();
+
+    return 0;
+}
