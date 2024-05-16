03f1540a39070c108b64110a40dc4677669e91b4
Makevich30 <84179265+Makevich30@users.noreply.github.com>
2024-05-17 01:12:30 +0400

    Create 16-1-4.cpp

---
+++ 16-1-4.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <fstream>
+#include <stack>
+#include <vector>
+
+using namespace std;
+
+void replaceElements(vector<int>& sequence, int x, int y) {
+    stack<int> stack;
+
+    for (int num : sequence) {
+        if (num != x) {
+            stack.push(num);
+        }
+    }
+
+    while (!stack.empty()) {
+        sequence[stack.size() - 1] = stack.top();
+        stack.pop();
+    }
+
+    if (!sequence.empty() && sequence.back() == x) {
+        sequence.back() = y;
+    }
+}
+
+int main() {
+    ifstream inputFile("input.txt");
+    ofstream outputFile("output.txt");
+
+    vector<int> sequence;
+    int x, y, num;
+
+    while (inputFile >> num) {
+        sequence.push_back(num);
+    }
+    inputFile >> x >> y;
+    
+    replaceElements(sequence, x, y);
+
+    for (int num : sequence) {
+        outputFile << num << " ";
+    }
+    outputFile << endl;
+
+    inputFile.close();
+    outputFile.close();
+
+    return 0;
+}
