1aef0c5985ad69319c39234336a73a4674674b06
Makevich30 <84179265+Makevich30@users.noreply.github.com>
2024-05-17 00:05:02 +0400

    Create 8-3-18.cpp

---
+++ 8-3-18.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+void replaceNegative(vector<int>& arr, int index = 0) {
+    if (index == arr.size())
+        return;
+
+    if (arr[index] < 0)
+        arr[index] *= -1;
+
+    replaceNegative(arr, index + 1);
+}
+
+int main() {
+    vector<int> arr;
+
+    ifstream inputFile("input.txt");
+    ofstream outputFile("output.txt");
+    
+    int num;
+    while (inputFile >> num) {
+        arr.push_back(num);
+    }
+
+    replaceNegative(arr);
+
+    for (int i = 0; i < arr.size(); ++i) {
+        outputFile << arr[i] << " ";
+    }
+    
+    inputFile.close();
+    outputFile.close();
+
+    return 0;
+}
